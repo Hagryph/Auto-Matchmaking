0c88cb35a4696806ea443570d9bd5ad4d68fad06
Yannis Hensel <yannis-jochen-hensel@hotmail.de>
2021-04-11 18:49:07 +0200

    AutoMM.py
    - added automatic pick/ban
    - added automatic requeue after game ends

--- AutoMM.py
+++ AutoMM.py
@@ -9,6 +9,7 @@ import pygetwindow as gw
 import threading as th
 
 #################################################################################################################################################################################################################
+
 protocol = 0
 host = '127.0.0.1'
 port = 0
@@ -20,11 +21,20 @@ threadLock = th.Lock()
 
 threads = {}
 kill = False
+
+champions = {"Annie":"1","Olaf":"2","Galio":"3","Twisted Fate":"4","Xin Zhao":"5","Urgot":"6","LeBlanc":"7","Vladimir":"8","Fiddlesticks":"9","Kayle":"10","Master Yi":"11","Alistar":"12","Ryze":"13","Sion":"14","Sivir":"15","Soraka":"16","Teemo":"17","Tristana":"18","Warwick":"19","Nunu":"20","Miss Fortune":"21","Ashe":"22","Tryndamere":"23","Jax":"24","Morgana":"25","Zilean":"26","Singed":"27","Evelynn":"28","Twitch":"29","Karthus":"30","Cho\'Gath":"31","Amumu":"32","Rammus":"33","Anivia":"34","Shaco":"35","Dr. Mundo":"36","Sona":"37","Kassadin":"38","Irelia":"39","Janna":"40","Gangplank":"41","Corki":"42","Karma":"43","Taric":"44","Veigar":"45","Trundle":"48","Swain":"50","Caitlyn":"51","Blitzcrank":"53","Malphite":"54","Katarina":"55","Nocturne":"56","Maokai":"57","Renekton":"58","Jarvan IV":"59","Elise":"60","Orianna":"61","Wukong":"62","Brand":"63","Lee Sin":"64","Vayne":"67","Rumble":"68","Cassiopeia":"69","Skarner":"72","Heimerdinger":"74","Nasus":"75","Nidalee":"76","Udyr":"77","Poppy":"78","Gragas":"79","Pantheon":"80","Ezreal":"81","Mordekaiser":"82","Yorick":"83","Akali":"84","Kennen":"85","Garen":"86","Leona":"89","Malzahar":"90","Talon":"91","Riven":"92","Kog\'Maw":"96","Shen":"98","Lux":"99","Xerath":"101","Shyvana":"102","Ahri":"103","Graves":"104","Fizz":"105","Volibear":"106","Rengar":"107","Varus":"110","Nautilus":"111","Viktor":"112","Sejuani":"113","Fiora":"114","Ziggs":"115","Lulu":"117","Draven":"119","Hecarim":"120","Kha\'Zix":"121","Darius":"122","Jayce":"126","Lissandra":"127","Diana":"131","Quinn":"133","Syndra":"134","Aurelion Sol":"136","Kayn":"141","Zoe":"142","Zyra":"143","Kai\'Sa":"145","Seraphine":"147","Gnar":"150","Zac":"154","Yasuo":"157","Vel\'Koz":"161","Taliyah":"163","Camille":"164","Braum":"201","Jhin":"202","Kindred":"203","Jinx":"222","Tahm Kench":"223","Senna":"235","Lucian":"236","Zed":"238","Kled":"240","Ekko":"245","Qiyana":"246","Vi":"254","Aatrox":"266","Nami":"267","Azir":"268","Yuumi":"350","Thresh":"412","Illaoi":"420","Rek\'Sai":"421","Ivern":"427","Kalista":"429","Bard":"432","Rakan":"497","Xayah":"498","Ornn":"516","Sylas":"517","Neeko":"518","Aphelios":"523","Pyke":"555","Yone":"777","Sett":"875","Lillia":"876"}
+
+banPrio = {}
+pickPrio = {}
+
+lastPhase = ""
+phase = ""
+
 #################################################################################################################################################################################################################
 
 def restoreConfig():
     file = open('config.json', 'w')
-    file.write('{"roles":["top", "mid"], "dir":"C://Riot Games//League of Legends"}')
+    file.write('{"roles":["mid", "top"], "dir":"C://Riot Games//League of Legends", "banPrio":{"mid":["Zed","Yasuo"],"top":["Aatrox","Yorick"],"jungle":["Master Yi","Nocturne"],"sup":["Morgana","Seraphine"],"bot":["Samira","Ashe"]}, "pickPrio":{"mid":["Yone","Yasuo"],"top":["Garen","Malphite"],"jungle":["Zac","Udyr"],"sup":["Lux","Brand"],"bot":["Caitlyn","Miss Fortune"]}}')
     file.close()
     print("Config restored pls check it and restart the script")
     sleep(5)
@@ -34,7 +44,7 @@ def restoreConfig():
 
 file = open('config.json','a+')
 if os.path.getsize('config.json') == 0:
-    file.write('{"roles":["mid", "top"], "dir":"C://Riot Games//League of Legends"}')
+    file.write('{"roles":["mid", "top"], "dir":"C://Riot Games//League of Legends", "banPrio":{"mid":["Zed","Yasuo"],"top":["Aatrox","Yorick"],"jungle":["Master Yi","Nocturne"],"sup":["Morgana","Seraphine"],"bot":["Samira","Ashe"]}, "pickPrio":{"mid":["Yone","Yasuo"],"top":["Garen","Malphite"],"jungle":["Zac","Udyr"],"sup":["Lux","Brand"],"bot":["Caitlyn","Miss Fortune"]}}')
 file.close()
 
 try:
@@ -45,6 +55,9 @@ except:
 
 
 role = {"sup": "UTILITY", "mid": "MIDDLE", "bot": "BOTTOM", "top": "TOP", "jungle": "JUNGLE", "fill": "FILL", "none": "NONE"}
+roleName = {}
+for key in role:
+    roleName[role[key].lower()] = key
 
 try:
     if config['roles'][0] == "fill":
@@ -81,6 +94,15 @@ except:
     restoreConfig()
 
 #################################################################################################################################################################################################################
+try:
+    for r in config['banPrio']:
+        banPrio[r] = []
+        for champ in config['banPrio'][r]:
+            banPrio[r].append(champions[champ])
+except:
+    pass
+
+#################################################################################################################################################################################################################
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -107,6 +129,22 @@ def request(method, path, query='', data=''):
         r = fn(url, verify=False, headers=headers, json=data)
 
     return r
+
+def getPickPrio():
+    global pickPrio
+    global champions
+
+    try:
+        availableChamps = request('get', '/lol-champions/v1/owned-champions-minimal').json()
+        for r in config['pickPrio']:
+            pickPrio[r] = []
+            for champ in config['pickPrio'][r]:
+                for aChamp in availableChamps:
+                    if int(aChamp['id']) == int(champions[champ]):
+                        if aChamp['rankedPlayEnabled']:
+                            pickPrio[r].append(aChamp['id'])
+    except:
+        resetConfig()
 
 def getRole():
     lobby = request('get', '/lol-lobby/v2/lobby').json()
@@ -136,21 +174,146 @@ def existsSearchError():
 def search():
     request('post','/lol-lobby/v2/lobby/matchmaking/search')
 
+def lobby():
+    if getRole() == 0:
+        print("Wrong Roles set...")
+        setRole()
+        print("Now your roles fit!")
+    else:
+        if leader() == 1:
+            if not existsSearchError():
+                search()
+                return True
+    return False
+
 def queue():
     request('post','/lol-lobby/v2/lobby','',{"queueId": 420})
 
 def readyCheck():
     request('post', '/lol-matchmaking/v1/ready-check/accept')
 
-def waitForEnd():
-    sleep(10)
-    return
+def getCellID():
+    session = request('get', '/lol-champ-select/v1/session').json()
+    return session['localPlayerCellId']
+
+def getPhase():
+    session = request('get', '/lol-champ-select/v1/session').json()
+    for actions in session['actions']:
+        for action in actions:
+            if action['isInProgress']:
+                return action['type']
+    return False
+
+def getPosition(ownCellID):
+    session = request('get', '/lol-champ-select/v1/session').json()
+
+    for player in session['myTeam']:
+        if player['cellId'] == ownCellID:
+            return player['assignedPosition']
+    return False
+
+def getBan(position):
+    global banPrio
+
+    if position:
+        prio = banPrio[roleName[position.lower()]]
+
+    session = request('get', '/lol-champ-select/v1/session').json()
+
+    bans = []
+
+    for action in session['actions'][0]:
+        if action['completed']:
+            bans.append(action['championId'])
+
+    for id in prio:
+        if not id in bans:
+            return id
+
+    return False
+
+def getBanID(ownCellID):
+    session = request('get', '/lol-champ-select/v1/session').json()
+    
+    for actions in session['actions']:
+        for action in actions:
+            if action['actorCellId'] == ownCellID:
+                if action['type'] == "ban":
+                    if action['isInProgress'] and not action['completed']:
+                        return action['id']
+    return False
+
+
+def ban(id, ownCellID, banID):
+    url = '/lol-champ-select/v1/session/actions/%d' % banID
+    data = {'championId': id}
+    request('patch', url, '', data)
+    request('post', url+'/complete', '', data)
+
+def getPickID(ownCellID):
+    session = request('get', '/lol-champ-select/v1/session').json()
+    
+    for actions in session['actions']:
+        for action in actions:
+            if action['actorCellId'] == ownCellID:
+                if action['type'] == "pick":
+                    if action['isInProgress'] and not action['completed']:
+                        return action['id']
+    return False
+
+def getPick(position):
+    global pickPrio
+
+    if position:
+        prio = pickPrio[roleName[position.lower()]]
+
+    session = request('get', '/lol-champ-select/v1/session').json()
+
+    notAvailable = []
+
+    for actions in session['actions']:
+        for action in actions:
+            if action['completed']:
+                notAvailable.append(action['championId'])
+
+    for id in prio:
+        if not id in notAvailable:
+            return id
+
+    return False
+
+def pick(id, ownCellID, pickID):
+    url = '/lol-champ-select/v1/session/actions/%d' % pickID
+    data = {'championId': id}
+    request('patch', url, '', data)
+    request('post', url+'/complete', '', data)
+
+def pick_ban():
+    phase = getPhase()
+    if phase:
+        ownCellID = getCellID()
+        if ownCellID != -1:
+            position = getPosition(ownCellID)
+            if phase == "ban":
+                banID = getBanID(ownCellID)
+                if banID:
+                    id = getBan(position)
+                    if id:
+                        ban(id, ownCellID, banID)
+            elif phase == "pick":
+                pickID = getPickID(ownCellID)
+                if pickID:
+                    id = getPick(position)
+                    if id:
+                        pick(id, ownCellID, pickID)
 
 #################################################################################################################################################################################################################
 
 def routine():
     global kill
     global threadLock
+    global lastPhase
+    global phase
 
     while True:
         sleep(1)
@@ -163,16 +326,14 @@ def routine():
         if r.json()['state'] == 'SUCCEEDED':
             window = gw.getWindowsWithTitle('League of Legends')[0]
             window.moveTo(500, 100)
-            pos_x = 1246*window.size[0]/1280
-            pos_y = 205*window.size[1]/720
             break
-        else:
-            print(r.json()['state'])
 
     summonerId = r.json()['summonerId']
 
     accepted = False
     searching = False
+    
+    getPickPrio()
 
     threadLock.acquire()
     while not kill:
@@ -182,41 +343,32 @@ def routine():
             r = request('get', '/lol-gameflow/v1/gameflow-phase')
 
             if r.status_code != 200:
-                print(Back.BLACK + Fore.RED + str(r.status_code) + Style.RESET_ALL, r.text)
+                print(str(r.status_code), r.text)
                 continue
 
             phase = r.json()
 
             if phase == "ReadyCheck":
-                searching = False
                 readyCheck()
-                if not accepted:
+                if phase != lastPhase:
                     print("Readycheck accepted")
-                    accepted = True
-            else:
-                accepted = False
 
-                if phase == "None":
-                    if not searching:
-                        print("Searching for Game")
-                        searching = True
-                    queue()
-                else:
-                    searching = False
+            elif phase == 'ChampSelect':
+                pick_ban()
 
-                    if phase == 'Lobby':
-                        if getRole() == 0:
-                            print("Wrong Roles set...")
-                            setRole()
-                            print("Now your roles fit!")
-                        else:
-                            if leader() == 1:
-                                if not existsSearchError():
-                                    search()
-                                    print("Started Queue as Leader")
+            elif phase == "None" or phase == "EndOfGame":
+                if phase != lastPhase:
+                    print("Searching for Game")
+                queue()
 
-                    elif phase == 'InProgress':
-                        waitForEnd()
+            elif phase == 'Lobby':
+                if lobby() and phase != lastPhase:
+                    print("Searching for game ...")
+
+            elif phase == 'InProgress':
+                sleep(10)
+
+            lastPhase = phase
         except:
             pass
         
@@ -250,7 +402,7 @@ def main():
 
                 print('Found running League of Legends, dir', config["dir"])
                 lockfile = open(r'%s\lockfile' % config["dir"], 'r')
-                sleep(5)
+                sleep(20)
         except:
             restoreConfig()
 
