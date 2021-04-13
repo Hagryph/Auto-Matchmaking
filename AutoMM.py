0d9a120424a60fbd5f976f5c92839d389f22e33d
Yannis Hensel <yannis-jochen-hensel@hotmail.de>
2021-04-13 09:58:47 +0200

    Add files via upload

--- AutoMM.py
+++ AutoMM.py
@@ -22,7 +22,7 @@ threadLock = th.Lock()
 threads = {}
 kill = False
 
-champions = {"Annie":"1","Olaf":"2","Galio":"3","Twisted Fate":"4","Xin Zhao":"5","Urgot":"6","LeBlanc":"7","Vladimir":"8","Fiddlesticks":"9","Kayle":"10","Master Yi":"11","Alistar":"12","Ryze":"13","Sion":"14","Sivir":"15","Soraka":"16","Teemo":"17","Tristana":"18","Warwick":"19","Nunu":"20","Miss Fortune":"21","Ashe":"22","Tryndamere":"23","Jax":"24","Morgana":"25","Zilean":"26","Singed":"27","Evelynn":"28","Twitch":"29","Karthus":"30","Cho\'Gath":"31","Amumu":"32","Rammus":"33","Anivia":"34","Shaco":"35","Dr. Mundo":"36","Sona":"37","Kassadin":"38","Irelia":"39","Janna":"40","Gangplank":"41","Corki":"42","Karma":"43","Taric":"44","Veigar":"45","Trundle":"48","Swain":"50","Caitlyn":"51","Blitzcrank":"53","Malphite":"54","Katarina":"55","Nocturne":"56","Maokai":"57","Renekton":"58","Jarvan IV":"59","Elise":"60","Orianna":"61","Wukong":"62","Brand":"63","Lee Sin":"64","Vayne":"67","Rumble":"68","Cassiopeia":"69","Skarner":"72","Heimerdinger":"74","Nasus":"75","Nidalee":"76","Udyr":"77","Poppy":"78","Gragas":"79","Pantheon":"80","Ezreal":"81","Mordekaiser":"82","Yorick":"83","Akali":"84","Kennen":"85","Garen":"86","Leona":"89","Malzahar":"90","Talon":"91","Riven":"92","Kog\'Maw":"96","Shen":"98","Lux":"99","Xerath":"101","Shyvana":"102","Ahri":"103","Graves":"104","Fizz":"105","Volibear":"106","Rengar":"107","Varus":"110","Nautilus":"111","Viktor":"112","Sejuani":"113","Fiora":"114","Ziggs":"115","Lulu":"117","Draven":"119","Hecarim":"120","Kha\'Zix":"121","Darius":"122","Jayce":"126","Lissandra":"127","Diana":"131","Quinn":"133","Syndra":"134","Aurelion Sol":"136","Kayn":"141","Zoe":"142","Zyra":"143","Kai\'Sa":"145","Seraphine":"147","Gnar":"150","Zac":"154","Yasuo":"157","Vel\'Koz":"161","Taliyah":"163","Camille":"164","Braum":"201","Jhin":"202","Kindred":"203","Jinx":"222","Tahm Kench":"223","Senna":"235","Lucian":"236","Zed":"238","Kled":"240","Ekko":"245","Qiyana":"246","Vi":"254","Aatrox":"266","Nami":"267","Azir":"268","Yuumi":"350","Thresh":"412","Illaoi":"420","Rek\'Sai":"421","Ivern":"427","Kalista":"429","Bard":"432","Rakan":"497","Xayah":"498","Ornn":"516","Sylas":"517","Neeko":"518","Aphelios":"523","Pyke":"555","Yone":"777","Sett":"875","Lillia":"876"}
+champions = {"Annie":"1","Olaf":"2","Galio":"3","Twisted Fate":"4","Xin Zhao":"5","Urgot":"6","LeBlanc":"7","Vladimir":"8","Fiddlesticks":"9","Kayle":"10","Master Yi":"11","Alistar":"12","Ryze":"13","Sion":"14","Sivir":"15","Soraka":"16","Teemo":"17","Tristana":"18","Warwick":"19","Nunu":"20","Miss Fortune":"21","Ashe":"22","Tryndamere":"23","Jax":"24","Morgana":"25","Zilean":"26","Singed":"27","Evelynn":"28","Twitch":"29","Karthus":"30","ChoGath":"31","Amumu":"32","Rammus":"33","Anivia":"34","Shaco":"35","Dr. Mundo":"36","Sona":"37","Kassadin":"38","Irelia":"39","Janna":"40","Gangplank":"41","Corki":"42","Karma":"43","Taric":"44","Veigar":"45","Trundle":"48","Swain":"50","Caitlyn":"51","Blitzcrank":"53","Malphite":"54","Katarina":"55","Nocturne":"56","Maokai":"57","Renekton":"58","Jarvan IV":"59","Elise":"60","Orianna":"61","Wukong":"62","Brand":"63","Lee Sin":"64","Vayne":"67","Rumble":"68","Cassiopeia":"69","Skarner":"72","Heimerdinger":"74","Nasus":"75","Nidalee":"76","Udyr":"77","Poppy":"78","Gragas":"79","Pantheon":"80","Ezreal":"81","Mordekaiser":"82","Yorick":"83","Akali":"84","Kennen":"85","Garen":"86","Leona":"89","Malzahar":"90","Talon":"91","Riven":"92","KogMaw":"96","Shen":"98","Lux":"99","Xerath":"101","Shyvana":"102","Ahri":"103","Graves":"104","Fizz":"105","Volibear":"106","Rengar":"107","Varus":"110","Nautilus":"111","Viktor":"112","Sejuani":"113","Fiora":"114","Ziggs":"115","Lulu":"117","Draven":"119","Hecarim":"120","KhaZix":"121","Darius":"122","Jayce":"126","Lissandra":"127","Diana":"131","Quinn":"133","Syndra":"134","Aurelion Sol":"136","Kayn":"141","Zoe":"142","Zyra":"143","KaiSa":"145","Seraphine":"147","Gnar":"150","Zac":"154","Yasuo":"157","VelKoz":"161","Taliyah":"163","Camille":"164","Braum":"201","Jhin":"202","Kindred":"203","Jinx":"222","Tahm Kench":"223","Viego":"234","Senna":"235","Lucian":"236","Zed":"238","Kled":"240","Ekko":"245","Qiyana":"246","Vi":"254","Aatrox":"266","Nami":"267","Azir":"268","Yuumi":"350","Samira":"360","Thresh":"412","Illaoi":"420","RekSai":"421","Ivern":"427","Kalista":"429","Bard":"432","Rakan":"497","Xayah":"498","Ornn":"516","Sylas":"517","Neeko":"518","Aphelios":"523","Rell":"526","Pyke":"555","Yone":"777","Sett":"875","Lillia":"876"}
 
 banPrio = {}
 pickPrio = {}
@@ -30,21 +30,25 @@ pickPrio = {}
 lastPhase = ""
 phase = ""
 
+standartConfig = '{"roles":["mid", "top"], "dir":"C://Riot Games//League of Legends", "autoChampSelect":"True", "banPrio":{"mid":["Zed","Yasuo"],"top":["Aatrox","Yorick"],"jungle":["Master Yi","Nocturne"],"sup":["Morgana","Seraphine"],"bot":["Samira","Ashe"]}, "pickPrio":{"mid":["Yone","Yasuo"],"top":["Garen","Malphite"],"jungle":["Zac","Udyr"],"sup":["Lux","Brand"],"bot":["Caitlyn","Miss Fortune"]}}'
+
 #################################################################################################################################################################################################################
 
 def restoreConfig():
+    global standartConfig
+
     file = open('config.json', 'w')
-    file.write('{"roles":["mid", "top"], "dir":"C://Riot Games//League of Legends", "banPrio":{"mid":["Zed","Yasuo"],"top":["Aatrox","Yorick"],"jungle":["Master Yi","Nocturne"],"sup":["Morgana","Seraphine"],"bot":["Samira","Ashe"]}, "pickPrio":{"mid":["Yone","Yasuo"],"top":["Garen","Malphite"],"jungle":["Zac","Udyr"],"sup":["Lux","Brand"],"bot":["Caitlyn","Miss Fortune"]}}')
+    file.write(standartConfig)
     file.close()
     print("Config restored pls check it and restart the script")
-    sleep(5)
-    exit()
+    sleep(10)
+    sys.exit()
 
 #################################################################################################################################################################################################################
 
 file = open('config.json','a+')
 if os.path.getsize('config.json') == 0:
-    file.write('{"roles":["mid", "top"], "dir":"C://Riot Games//League of Legends", "banPrio":{"mid":["Zed","Yasuo"],"top":["Aatrox","Yorick"],"jungle":["Master Yi","Nocturne"],"sup":["Morgana","Seraphine"],"bot":["Samira","Ashe"]}, "pickPrio":{"mid":["Yone","Yasuo"],"top":["Garen","Malphite"],"jungle":["Zac","Udyr"],"sup":["Lux","Brand"],"bot":["Caitlyn","Miss Fortune"]}}')
+    file.write(standartConfig)
 file.close()
 
 try:
@@ -54,7 +58,7 @@ except:
     restoreConfig()
 
 
-role = {"sup": "UTILITY", "mid": "MIDDLE", "bot": "BOTTOM", "top": "TOP", "jungle": "JUNGLE", "fill": "FILL", "none": "NONE"}
+role = {"sup": "UTILITY", "mid": "MIDDLE", "bot": "BOTTOM", "top": "TOP", "jungle": "JUNGLE", "fill": "FILL"}
 roleName = {}
 for key in role:
     roleName[role[key].lower()] = key
@@ -64,15 +68,16 @@ try:
         config['roles'][1] = "fill"
 
     changed = False
+    keys = list(role.keys())
 
-    if config['roles'][0] != "mid" and config['roles'][0] != "top" and config['roles'][0] != "bot" and config['roles'][0] != "jungle" and config['roles'][0] != "sup":
+    if config['roles'][0] not in keys:
         if config['roles'][1] != "mid":
             config['roles'][0] = "mid"
         else:
             config['roles'][0] = "top"
         changed = True
 
-    if config['roles'][1] != "mid" and config['roles'][1] != "top" and config['roles'][1] != "bot" and config['roles'][1] != "jungle" and config['roles'][1] != "sup":
+    if config['roles'][1] not in keys:
         if config['roles'][0] != "mid":
             config['roles'][1] = "mid"
         else:
@@ -83,8 +88,8 @@ try:
         with open('config.json', 'w') as f:
             json.dump(config, f)
         print("Config restored pls check it and restart the script")
-        sleep(5)
-        exit()
+        sleep(10)
+        sys.exit()
      
     rolePrio = [
         role[config['roles'][0]],
@@ -94,13 +99,15 @@ except:
     restoreConfig()
 
 #################################################################################################################################################################################################################
+
 try:
-    for r in config['banPrio']:
-        banPrio[r] = []
-        for champ in config['banPrio'][r]:
-            banPrio[r].append(champions[champ])
+    if config['autoChampSelect'] == "True":
+        for r in config['banPrio']:
+            banPrio[r] = []
+            for champ in config['banPrio'][r]:
+                banPrio[r].append(champions[champ])
 except:
-    pass
+    restoreConfig()
 
 #################################################################################################################################################################################################################
 
@@ -324,6 +331,11 @@ def routine():
             continue
 
         if r.json()['state'] == 'SUCCEEDED':
+            try:
+                window = gw.getWindowsWithTitle('League of Legends')[0]
+                window.moveTo(500, 100)
+            except:
+                pass
             break
 
     summonerId = r.json()['summonerId']
@@ -331,7 +343,11 @@ def routine():
     accepted = False
     searching = False
     
-    getPickPrio()
+    try:
+        if config['autoChampSelect'] == "True":
+            getPickPrio()
+    except:
+        restoreConfig()
 
     threadLock.acquire()
     while not kill:
@@ -352,16 +368,20 @@ def routine():
                     print("Readycheck accepted")
 
             elif phase == 'ChampSelect':
-                pick_ban()
+                try:
+                    if config['autoChampSelect'] == "True":
+                        pick_ban()
+                except:
+                    restoreConfig()
 
             elif phase == "None" or phase == "EndOfGame":
                 if phase != lastPhase:
-                    print("Searching for Game")
+                    print("Entering Lobby")
                 queue()
 
             elif phase == 'Lobby':
                 if lobby() and phase != lastPhase:
-                    print("Searching for game ...")
+                    print("Searching for Game")
 
             elif phase == 'InProgress':
                 sleep(10)
@@ -410,12 +430,15 @@ def main():
         lock = lockdata.split(':')
 
         procname = lock[0]
-        pid = lock[1]
 
-        protocol = lock[4]
+        pid = lock[1]
+        
         port = lock[2]
 
         password = lock[3]
+
+        protocol = lock[4]
+
     
         userpass = b64encode(bytes('%s:%s' % (username, password), 'utf-8')).decode('ascii')
         headers = { 'Authorization': 'Basic %s' % userpass }
